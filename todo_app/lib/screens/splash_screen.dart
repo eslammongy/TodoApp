423f09437d142e9da653b40530604e4efcfb9266
eslam <eslammongy995@gmail.com>
2021-12-02 00:11:54 +0200

    finishing splash screen ui && login

--- todo_app/lib/screens/splash_screen.dart
+++ todo_app/lib/screens/splash_screen.dart
@@ -16,10 +16,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    /*  timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+    timer = Timer.periodic(const Duration(seconds: 3), (timer) {
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-    }); */
+    });
   }
 
   @override
@@ -28,22 +28,15 @@ class _SplashScreenState extends State<SplashScreen> {
       backgroundColor: const Color(0xff363636),
       body: Container(
         decoration: const BoxDecoration(
-            gradient: RadialGradient(
-                radius: 0.8,
-                focalRadius: 0.1,
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                stops: [0.0, 0.4, 1.0],
                 colors: [Colors.black, Colors.black45, Colors.black54])),
         child: Column(
           children: [
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.20,
-            ),
-            const Text(
-              'Helper Me',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25,
-                  fontWeight: FontWeight.w800,
-                  fontFamily: "Island Namina"),
             ),
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.10,
@@ -51,19 +44,31 @@ class _SplashScreenState extends State<SplashScreen> {
             Container(
               padding: EdgeInsets.zero,
               child: Image.asset(
-                'assets/images/task_done.gif',
-                fit: BoxFit.cover,
+                'assets/images/helper_logo.png',
+                fit: BoxFit.contain,
                 width: double.infinity,
-                height: 140,
+                height: 150,
               ),
             ),
+            const Text(
+              'My Helper',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w800,
+                  fontFamily: "Island Namina"),
+            ),
+            const Spacer(),
             const Text(
               'Manage Your Task',
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 16,
-                  fontWeight: FontWeight.w800,
-                  fontFamily: "Island Namina"),
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Roboto"),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
             ),
           ],
         ),
