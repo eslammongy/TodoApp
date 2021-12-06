214e057d392a672d65ebd2204cd928dcca50ea72
eslam <eslammongy995@gmail.com>
2021-12-06 13:46:30 +0200

    build home screen taskBar

--- todo_app/lib/screens/home_screen.dart
+++ todo_app/lib/screens/home_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:todo_app/constants/size_config.dart';
 import 'package:todo_app/constants/theme.dart';
+import 'package:todo_app/controllers/task_controller.dart';
 import 'package:todo_app/screens/add_new_task.dart';
 import 'package:todo_app/screens/widgets/custom_appbar.dart';
-import 'package:todo_app/screens/widgets/input_from_field.dart';
 import 'package:todo_app/services/theme_services.dart';
 import 'widgets/custom__button.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+  final TaskController taskController = Get.put(TaskController());
 
   @override
   Widget build(BuildContext context) {
@@ -26,22 +28,42 @@ class HomeScreen extends StatelessWidget {
                     : Icons.nightlight_round_outlined,
                 color: Get.isDarkMode ? orangeClr : darkGreyClr,
               ))),
-      body: SingleChildScrollView(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        children: [buildTaskBar()],
+      ),
+    );
+  }
+
+  Widget buildTaskBar() {
+    return Container(
+      margin: const EdgeInsets.only(top: 10, left: 20, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Center(
-            child: CustomButton(
-              label: "Click Me",
-              onTap: () {
-                Get.to(const AddNewTask());
-              },
-              height: 50,
-              width: 150,
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateFormat.yMMMMd().format(DateTime.now()),
+                style: titleStyle,
+              ),
+              Text(
+                "Today",
+                style: subHeadingStyle,
+              )
+            ],
+          ),
+          CustomButton(
+            label: "+ Add Task",
+            onTap: () async {
+              await Get.to(() => const AddNewTask());
+              taskController.getAllTasks();
+            },
+            height: 50,
+            width: 130,
           ),
         ],
-      )),
+      ),
     );
   }
 }
